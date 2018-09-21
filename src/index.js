f9cb6e9c56545ab75d5b90df4d4e15076dce8446
MaryiaSiamenava <maryiasiamenava@gmail.com>
2018-09-21 23:11:48 +0300

    love-triangle/solution

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let trianglesCount = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let angleA = preferences[i];
+    let angleB = preferences[angleA - 1];
+    let angleC = preferences[angleB - 1];
+    if (angleA != angleB && angleB != angleC && angleA > (i + 1) && angleB > (i + 1) && angleC == (i + 1)) {
+      trianglesCount++;
+    }
+  }
+  return trianglesCount;
+}
